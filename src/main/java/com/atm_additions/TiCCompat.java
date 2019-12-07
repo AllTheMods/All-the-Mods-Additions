377dd71b4aa8272c1c5bd1e6aa10d846a002afa2
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-12-06 20:36:10 -0500

    fixed soldering alloy tic material color

--- src/main/java/com/atm_additions/TiCCompat.java
+++ src/main/java/com/atm_additions/TiCCompat.java
@@ -12,7 +12,7 @@ public class TiCCompat {
 
     public static void initMaterials() {
         registerMaterial("vitrified_sand", 0xf7e4a1, true, true, Registry.vitrifiedSand, new HeadMaterialStats(400, 5.5F, 5.5F, 2), new HandleMaterialStats(0.5F, 400), new BowStringMaterialStats(0.5F));
-        registerMaterial("soldering_alloy", 0xf7e4a1, true, true, Registry.solderingAlloy, new HeadMaterialStats(1000, 7.5F, 7.5F, 3), new HandleMaterialStats(0.7F, 500), new BowStringMaterialStats(0.5F));
+        registerMaterial("soldering_alloy", 0xbbbec0, true, true, Registry.solderingAlloy, new HeadMaterialStats(1000, 7.5F, 7.5F, 3), new HandleMaterialStats(0.7F, 500), new BowStringMaterialStats(0.5F));
     }
 
     protected static void registerMaterial(String name, int color, boolean castable, boolean craftable, Fluid fluid, IMaterialStats stats, IMaterialStats... stats2) {
